6cfc9c58d568c9a61f4c3a021df96a28651df261
Victoria Akpan <veedahv55@gmail.com>
2022-07-19 23:29:16 +0100

    project update

--- src/views/auth/SignIn.vue
+++ src/views/auth/SignIn.vue
@@ -1,105 +1,196 @@
 <template>
   <ion-page>
     <ion-content :fullscreen="true">
-        <div class="px-8">
-
-      <ion-grid>
-        <ion-row class="ion-align-items-end">
-          <ion-col size="12" size-sm>
-            <div class="max-w-xs mx-auto">
-                  <div class="px-4 py-5 text-center relative">
-                    <div class="my-5">
-                      <div class="my-3">
-                        <div class="">
-                          <ion-img
-                            :src="require('@/assets/firstboard.png')"
-                            class="w-4"
-                          ></ion-img>
-                        </div>
-                        <!-- <h1>Slide 1</h1> -->
-                        <ion-title class="px-0 my-2 font-bold text-3xl">
-                          Welcome Back!
-                        </ion-title>
-                        <p class="text-[#4838D1] text-sm font-normal">
-                          Fill your details to log in
-                        </p>
-                      </div>
+      <div class="px-8">
+        <ion-grid>
+          <ion-row class="ion-align-items-end">
+            <ion-col size="12" size-sm>
+              <div class="max-w-xs mx-auto">
+                <div class="py-5 relative">
+                  <div class="my-5">
+                    <div class="my-3">
+                      <ion-title class="px-0 my-2 font-bold text-3xl">
+                        Welcome Back!
+                      </ion-title>
+                      <p class="text-[#4838D1] text-sm font-normal">
+                        Fill your details to log in
+                      </p>
                     </div>
                   </div>
-              <div class="flex justify-center gap-3 my-3">
-                <!-- <ion-button color="light" class="w-3 h-3 p-0 rounded-full overflow-hidden"> &nbsp; </ion-button> -->
-                <ion-button fill="clear" class="text-white flex-1">
-                  Skip
-                </ion-button>
-                <ion-button
-                  fill="clear"
-                  class="
-                    text-sm text-white
-                    bg-[#4838D1] bg-opacity-50
-                    rounded-md
-                    flex-1
-                  "
-                >
+                </div>
+                <form class="form" @submit.prevent="login">
+                  <div
+                    class="
+                      bg-white
+                      px-4
+                      my-4
+                      gap-3
+                      rounded-md
+                      overflow-hidden
+                      flex
+                      items-center
+                      flex-nowrap
+                      input-group
+                    "
+                  >
+                    <label for="name">
+                      <ion-img
+                        :src="require('@/assets/auth/user.svg')"
+                        class="w-4"
+                      ></ion-img>
+                    </label>
+                    <input
+                      placeholder="Name"
+                      class="
+                        bg-white
+                        text-dark
+                        w-full
+                        flex-auto
+                        outline-none
+                        py-3
+                      "
+                      color="dark"
+                      v-model="name"
+                      id="name"
+                    />
+                  </div>
+                  <div
+                    class="
+                      bg-white
+                      px-4
+                      my-4
+                      gap-3
+                      rounded-md
+                      overflow-hidden
+                      flex
+                      items-center
+                      flex-nowrap
+                      input-group
+                    "
+                  >
+                    <label for="email">
+                      <ion-img
+                        :src="require('@/assets/auth/mail.svg')"
+                        class="w-4"
+                      ></ion-img>
+                    </label>
+                    <input
+                      class="
+                        bg-white
+                        text-dark
+                        w-full
+                        flex-auto
+                        outline-none
+                        py-3
+                      "
+                      color="dark"
+                      v-model="email"
+                      placeholder="Email Address"
+                      id="email"
+                    />
+                  </div>
+                  <div
+                    class="
+                      bg-white
+                      px-4
+                      my-4
+                      gap-3
+                      rounded-md
+                      overflow-hidden
+                      flex
+                      items-center
+                      flex-nowrap
+                      input-group
+                    "
+                  >
+                    <label for="password">
+                      <ion-img
+                        :src="require('@/assets/auth/lock.svg')"
+                        class="w-4"
+                      ></ion-img>
+                    </label>
+                    <input
+                      placeholder="Password"
+                      class="
+                        bg-white
+                        text-dark
+                        w-full
+                        flex-auto
+                        outline-none
+                        py-3
+                      "
+                      color="dark"
+                      v-model="password"
+                      id="password"
+                    />
+                    <button class="">
+                      <ion-img
+                        :src="require('@/assets/auth/eye-slash.svg')"
+                        class="w-4"
+                      ></ion-img>
+                      <!-- @click="slidesRef.slideNext()" -->
+                      <!-- @click="slidesRef.value.slideNext()" -->
+                      <!-- Next -->
+                    </button>
+                  </div>
+                  <div class="">
+                    <a href="" class=""> Forget password? </a>
+                  </div>
+                  <div class="flex justify-center gap-3 my-3">
+                    <ion-button
+                      fill="clear"
+                      class="
+                        text-sm text-white
+                        bg-[#4838D1] bg-opacity-50
+                        rounded-md
+                        flex-1
+                      "
+                    >
+                      <!-- @click="slidesRef.slideNext()" -->
+                      <!-- @click="slidesRef.value.slideNext()" -->
+                      Login
+                    </ion-button>
+                  </div>
+                </form>
+                <div class="flex justify-center gap-3 my-3">
+                  <!-- <ion-button color="light" class="w-3 h-3 p-0 rounded-full overflow-hidden"> &nbsp; </ion-button> -->
+                  <ion-button
+                    fill="clear"
+                    class="bg-white flex-1 rounded-lg overflow-hidden"
+                  >
+                    Skip
+                  </ion-button>
+                  <ion-button fill="clear" class="bg-white flex-1">
+                    Skip
+                  </ion-button>
+                  <ion-button fill="clear" class="bg-white flex-1">
+                    Skip
+                  </ion-button>
+                  <!-- <ion-button
+                    fill="clear"
+                    class="
+                      text-sm text-white
+                      bg-[#4838D1] bg-opacity-50
+                      rounded-md
+                      flex-1
+                    "
+                  > -->
                   <!-- @click="slidesRef.slideNext()" -->
                   <!-- @click="slidesRef.value.slideNext()" -->
-                  Next
-                </ion-button>
+                  <!-- Next
+                  </ion-button> -->
+                </div>
+                <div class="">
+                  <p class="">
+                    Don't have an account?
+                    <a href="" class=""> Register </a>
+                  </p>
+                </div>
               </div>
-            </div>
-            <!-- ion-col [size="12"] [size-sm] -->
-          </ion-col>
-        </ion-row>
-      </ion-grid>
-      <!-- Default Input -->
-      <ion-input></ion-input>
-
-      <!-- Input with value -->
-      <ion-input value="custom"></ion-input>
-
-      <!-- Input with placeholder -->
-          <div class="flex items-center bg-white px-4 rounded-md overflow-hidden">
-        <ion-label color="primary">Default</ion-label>
-            <ion-input
-              placeholder="Search Books or Author..."
-              class="bg-white text-dark"
-              color="dark"
-            ></ion-input>
-          </div>
-      <ion-input placeholder="Enter Input"></ion-input>
-
-      <!-- Input with clear button when there is a value -->
-      <ion-input clear-input value="clear me"></ion-input>
-
-      <!-- Number type input -->
-      <ion-input type="number" value="333"></ion-input>
-
-      <!-- Disabled input -->
-      <ion-input value="Disabled" disabled></ion-input>
-
-      <!-- Readonly input -->
-      <ion-input value="Readonly" readonly></ion-input>
-
-      <!-- Inputs with labels -->
-      <ion-item>
-        <ion-label>Default Label</ion-label>
-        <ion-input></ion-input>
-      </ion-item>
-
-      <ion-item>
-        <ion-label position="floating">Floating Label</ion-label>
-        <ion-input></ion-input>
-      </ion-item>
-
-      <ion-item>
-        <ion-label position="fixed">Fixed Label</ion-label>
-        <ion-input></ion-input>
-      </ion-item>
-
-      <ion-item>
-        <ion-label position="stacked">Stacked Label</ion-label>
-        <ion-input></ion-input>
-      </ion-item>
-        </div>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -121,9 +212,9 @@ import {
   IonCol,
   IonGrid,
   IonRow,
-  IonLabel,
-  IonInput,
-  IonItem,
+  // IonLabel,
+  // IonInput,
+  // IonItem,
   //   IonToolbar,
 } from "@ionic/vue";
 
@@ -142,14 +233,18 @@ export default defineComponent({
     IonCol,
     IonGrid,
     IonRow,
-    IonLabel,
-    IonInput,
-    IonItem,
+    // IonLabel,
+    // IonInput,
+    // IonItem,
   },
   data() {
     return {
       //   lastSlide: false
       // slideSetup: {}
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
     };
   },
   setup() {
@@ -188,21 +283,17 @@ export default defineComponent({
       lastSlide,
     };
   },
-  //   mounted() {
+    mounted() {
+      console.log(this.$router.currentRoute);
   //     this.slideSetup = this.slidesRef;
-  //   },
-  methods: {},
+    },
+  methods: {
+    login() {
+      console.log("email: ", this.email);
+    },
+  },
 });
 </script>
 
 <style scoped>
-.swiper {
-  /* height: 300px; */
-
-  /* --bullet-background: rgb(var(--ion-color-primary-rgb), 0.5);
-  --bullet-background-active: var(--ion-color-primary); */
-  --bullet-background: rgba(8, 28, 77, 0.9);
-  --bullet-background-active: rgba(72, 56, 209, 0.9);
-  --scroll-bar-background: var(--ion-color-light);
-}
 </style>
